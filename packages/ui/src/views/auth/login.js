570258b5b77e88a9cd8f34eb8576bb3938375652
sham <shamfreelance@gmail.com>
2024-02-28 04:45:51 +0500

    errors res

--- packages/ui/src/views/auth/login.js
+++ packages/ui/src/views/auth/login.js
@@ -54,15 +54,16 @@ export default function LoginView() {
                 </Typography>
                 <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
                     <TextField margin='normal' required fullWidth id='email' label='Email Address' name='email' autoComplete='email' />
-                    <TextField 
-                        margin='normal' 
-                        required fullWidth 
-                        name='password' 
-                        label='Password' 
-                        type='password' 
-                        id='password' 
+                    <TextField
+                        margin='normal'
+                        required 
+                        fullWidth
+                        name='password'
+                        label='Password'
+                        type='password'
+                        id='password'
                         autoComplete='current-password'
-                        />
+                    />
                     <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me' />
 
                     {error && (
