8cf23d693766bbceeade6a64d191b91f1c690d62
sham <shamfreelance@gmail.com>
2024-03-10 13:48:11 +0500

    mfa err rm

--- packages/ui/src/views/auth/login.js
+++ packages/ui/src/views/auth/login.js
@@ -83,7 +83,7 @@ export default function LoginView() {
                     <Grid container>
                         <Grid item>
                             <Link to='/signup' variant='body2'>
-                                {'Don't have an account? Sign Up'}
+                                {"Don't have an account? Sign Up'"}
                             </Link>
                         </Grid>
                     </Grid>
